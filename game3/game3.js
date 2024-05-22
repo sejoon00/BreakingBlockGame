f24c3c56baf85f063801f7fb54230918945c013e
eunseo <songes04@gmail.com>
2024-05-22 13:03:06 +0900

    Canvas3 init

--- game3/game3.js
+++ game3/game3.js
@@ -6,3 +6,25 @@ document.querySelector("main").insertAdjacentHTML(
       </div>
       `
 );
+
+// ------------------------------------ javascript ------------------------------------
+/* 해당 페이지의 javascript 코드를 작성하고 삽압하는 구간입니다.*/
+let canvas3;
+
+// ---------------------------------- javascript function ----------------------------------
+/* 해당 페이지의 javascript에서 사용하는 function을 정의하는 구간입니다.*/
+function setGame3() {
+  // 특정 요소를 선택합니다.
+  let game3 = document.querySelector('#game3');
+
+  // 요소 내의 모든 자식 요소를 제거합니다.
+
+  // 새로운 Canvas 객체를 생성하고 해당 요소에 추가합니다.
+  canvas2 = new Canvas3('./game3/game3Background.png');
+  canvas2.appendTo(game2);
+}
+
+function endGame2() {
+  let game1 = document.querySelector('#game3');
+  canvas2.removeFrom(game1);
+}
