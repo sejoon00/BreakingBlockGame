ea321d14836c3b2162ca00b84ee6c05cebf74c68
김민석 <skyfox001015@naver.com>
2024-05-24 19:38:25 +0900

    feat : 바닐로페 움직이기, 생명 감소 완료

--- component/Canvas2.js
+++ component/Canvas2.js
@@ -14,6 +14,11 @@ class Canvas2 {
     this.lifes = []; // 생명 초기화
     this.score = 0; // 점수 초기화
     this.brokenBlocks = 0; // 부서진 블록 수 초기화
+    this.vanellope;
+    this.blockSpeed = 0.7; // 블록의 이동 속도
+    this.moveState = 0; // 이동 상태 (0: 아래로, 1: 왼쪽으로, 2: 위로, 3: 왼쪽으로)
+    this.maxDistance = 200; // 한 번에 이동할 최대 거리
+    this.currentDistance = 0; // 현재 이동 거리
 
     window.addEventListener('resize', this.resizeCanvas.bind(this));
     this.backgroundimage.onload = () => {
@@ -29,9 +34,6 @@ class Canvas2 {
     this.backgroundimage.src = backgroundimageUrl;
   }
 
-  // 주석
-  // game2 init
-
   drawBackground() {
     this.context.drawImage(
       this.backgroundimage,
@@ -41,10 +43,10 @@ class Canvas2 {
       this.canvas.height
     );
   }
+
   resizeCanvas() {
     let gameWidth = window.getComputedStyle(document.querySelector('#game2'));
 
-    console.log(gameWidth.width);
     this.canvas.width = parseFloat(gameWidth.width);
     this.canvas.height = this.canvas.width * 0.75;
     this.drawBackground(); // 캔버스 크기가 변경될 때마다 배경 다시 그리기
@@ -64,35 +66,26 @@ class Canvas2 {
     element.removeChild(this.canvas);
   }
 
-  // 점수 증가 함수
   increaseScore() {
-    this.score += 100; // 점수를 10 증가시킵니다.
+    this.score += 100; // 점수를 100 증가시킵니다.
     console.log('score');
   }
+
   decreaseLife() {
     console.log('생명-1');
-
     if (this.lifes.length > 0) {
       this.lifes.pop(); // 생명 배열에서 하나를 제거합니다.
     }
   }
 
-  // 점수를 화면에 표시하는 함수
   drawScore() {
     this.context.font = '24px Arial';
     this.context.fillStyle = 'yellow';
-    // this.context.strokeStyle = "black";
     this.context.lineWidth = 2;
     this.context.fillText('Score: ' + this.score, this.canvas.width - 150, 35);
-    // this.context.strokeText(
-    //   "Score: " + this.score,
-    //   this.canvas.width - 150,
-    //   50
-    // );
   }
 
   destroy() {
-    // 리소스 정리 코드
     window.removeEventListener('resize', this.resizeCanvas.bind(this));
     this.balls = [];
     this.blocks = [];
@@ -101,17 +94,25 @@ class Canvas2 {
     this.lifes = [];
   }
 
-  //게임 내 요소들을 초기화합니다.
   initGameElements() {
     this.balls.push(
       new Ball(this.ballInitialX, this.ballInitialY, 1, -1, 10, '#0095DD')
     );
-    console.log(this.balls);
     this.paddle = new Paddle(this.canvas, 100, 10, 10);
-
     this.paddle.bindMouseMove();
 
-    // 생명 배치
+    let startY = 116;
+    let startX = 900; // 블록의 시작 x 위치
+    this.vanellope = new Block(
+      startX,
+      startY,
+      60,
+      40,
+      this.increaseBrokenBlocks.bind(this),
+      '../game2/vanellope.png'
+    );
+    this.vanellope.isVanellope = true;
+
     for (let i = 0; i < 3; i++) {
       this.lifes.push(
         new Life(this.canvas, '../source/full_heart.png', 30, 10 + i * 40, 10)
@@ -119,13 +120,11 @@ class Canvas2 {
     }
   }
 
-  //공 개수 증가 아이템을 먹을시 추가 공을 화면에 그리는 함수
   addBall(dx, dy, radius, color) {
     const newBall = new Ball(x, y, dx, dy, radius, color);
     this.balls.push(newBall);
   }
 
-  // 부서진 블록 수 증가 함수
   increaseBrokenBlocks() {
     this.brokenBlocks++;
     if (this.brokenBlocks === this.blocks.length) {
@@ -133,16 +132,45 @@ class Canvas2 {
     }
   }
 
-  //화면에 요소들을 일정주기마다 다시 그립니다.
   startGameLoop() {
     const update = () => {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.drawBackground(); // 배경 다시 그리기
-      this.blocks.forEach((block) => {
-        block.draw(this.context); // 블록 그리기
+
+      // 블록 이동 로직 추가
+      if (this.moveState === 0) {
+        this.vanellope.y += this.blockSpeed; // 아래로 이동
+      } else if (this.moveState === 1) {
+        this.vanellope.x -= this.blockSpeed; // 왼쪽으로 이동
+      } else if (this.moveState === 2) {
+        this.vanellope.y -= this.blockSpeed; // 위로 이동
+      } else if (this.moveState === 3) {
+        this.vanellope.x -= this.blockSpeed; // 왼쪽으로 이동
+      }
+
+      this.currentDistance += this.blockSpeed;
+
+      if (this.currentDistance >= this.maxDistance) {
+        this.moveState = (this.moveState + 1) % 4; // 이동 상태 변경
+        this.currentDistance = 0; // 이동 거리 초기화
+      }
+
+      this.vanellope.draw(this.context);
+
+      // 공과 vanellope 블록의 충돌 검사
+      this.balls.forEach((ball) => {
+        if (
+          this.vanellope.isHit(ball, this.items, this.increaseScore.bind(this))
+        ) {
+          this.vanellope.visible = true;
+          this.decreaseLife(); // 생명 하나 줄이기
+        }
       });
 
-      //민석 - 공이 땅에 떨어진거 필터링해서 다시 공 배열 업데이트해줌
+      if (this.vanellope.x + this.vanellope.width < 0) {
+        alert('게임 승리');
+      }
+
       this.balls = this.balls.filter((ball) => {
         ball.update(
           this.canvas,
@@ -154,15 +182,6 @@ class Canvas2 {
         );
         return !ball.isRemoved;
       });
-      //민석 - 공이 땅에 떨어지면 생명 배열에서 삭제
-      // const numRemovedBalls = this.balls.reduce((acc, ball) => {
-      //   if (ball.isRemoved) {
-      //     acc++;
-      //   }
-      //   return acc;
-      // }, 0);
-
-      // this.lifes.splice(0, numRemovedBalls);
 
       this.balls.forEach((ball) => {
         ball.draw(this.context);
