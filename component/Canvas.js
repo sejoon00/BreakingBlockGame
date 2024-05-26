50078f08b111f1db12e237c7ec495004d2e8f8d8
kgonyoung <kgonyoung@gmail.com>
2024-05-26 16:42:48 +0900

    canvas2.js에 속도 아이템 관련 함수,canvas.js에 생명관련함수

--- component/Canvas.js
+++ component/Canvas.js
@@ -67,6 +67,16 @@ class Canvas {
       this.lifes.pop(); // 생명 배열에서 하나를 제거합니다.
     }
   }
+  // 곤용 increaseLife 
+  increaseLife() {
+    console.log("생명+1");
+  
+    const newX = 10 + this.lifes.length * 40;
+    
+    const newLife = new Life(this.canvas, "../source/full_heart.png", 30, newX, 10);
+  
+    this.lifes.push(newLife);
+  }
 
   drawScore() {
     // 첫 번째 줄 스타일
