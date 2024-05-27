9234a01004f82019b1d1ff0d17678f10a5ae10a3
김민석 <skyfox001015@naver.com>
2024-05-27 17:06:33 +0900

    feat : 몬스터,빌런 이동경로 수정

--- game2/Canvas2.js
+++ game2/Canvas2.js
@@ -5,9 +5,9 @@ class Canvas2 extends Canvas {
     this.vanellope;
     this.villains = []; // 빌런 리스트 초기화
     this.blockSpeed = 0.5; // 블록의 이동 속도
-    this.vanellopeState = { moveState: 0, currentDistance: 0 }; // 바닐로페 이동 상태 및 거리
-    this.villainStates = new Map(); // 빌런 이동 상태 및 거리
+    this.moveState = 0; // 이동 상태 (0: 오른쪽 아래 대각선, 1: 왼쪽 아래 대각선, 2: 오른쪽 위 대각선, 3: 왼쪽 위 대각선)
     this.maxDistance = 100; // 한 번에 이동할 최대 거리
+    this.currentDistance = 0; // 현재 이동 거리
     this.bananas = []; // 바나나 초기화
     this.frozenBlocks = new Set(); // 일시정지된 블록들
     this.isWarningVisible = false; // 경고 메시지 표시 여부
@@ -28,9 +28,7 @@ class Canvas2 extends Canvas {
       '../game2/vanellope.png'
     );
     this.vanellope.isVanellope = true;
-    //곤용 originalSpeed적용 두 줄 추가
-    // 기존속도 ,변경 속도 콘솔창 undefined => 정상적으로 출력
-    this.vanellope.blockSpeed = this.blockSpeed; 
+    this.vanellope.blockSpeed = this.blockSpeed;
     this.vanellope.originalBlockSpeed = this.blockSpeed;
 
     for (let i = 0; i < 6; i++) {
@@ -43,9 +41,7 @@ class Canvas2 extends Canvas {
         '../game2/villain' + i + '.png',
         false // 일반 블록임을 나타내는 플래그
       );
-      //곤용 originalSpeed적용 두 줄 추가
-      // 기존속도 ,변경 속도 콘솔창 undefined => 정상적으로 출력
-      villainBlock.blockSpeed = this.blockSpeed; 
+      villainBlock.blockSpeed = this.blockSpeed;
       villainBlock.originalBlockSpeed = this.blockSpeed;
       this.villains.push(villainBlock);
       this.villainStates.set(villainBlock, {
@@ -149,7 +145,6 @@ class Canvas2 extends Canvas {
       game3Img.src = '../stagePage/HeroDuty.png';
     }
   }
-  /*
   startGameLoop() {
     const update = () => {
       if (this.isPaused) return; // 게임이 일시 중지된 경우 업데이트 중지
@@ -246,11 +241,11 @@ class Canvas2 extends Canvas {
       });
 
       this.items.forEach((item) => {
-        this.balls.forEach((ball) => this.collectItem2(item, ball, this.balls));
+        this.balls.forEach((ball) => this.collectItem2(item, ball));
       });
 
-      // 바나나 그리기 및 충돌 처리
-      this.bananas = this.bananas.filter((banana) => {
+      // 바나나 그리기
+      this.bananas.forEach((banana) => {
         banana.draw();
         let collisionDetected = false;
 
@@ -286,146 +281,6 @@ class Canvas2 extends Canvas {
       this.endGame();
     };
     update();
-  }
-  */
-  startGameLoop() {
-    const update = () => {
-      if (this.isPaused) return; // 게임이 일시 중지된 경우 업데이트 중지
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.drawBackground(); // 배경 다시 그리기
-  
-      // vanellope 이동 로직
-      if (this.moveState === 0) {
-        this.vanellope.y += this.vanellope.blockSpeed; // 아래로 이동
-      } else if (this.moveState === 1) {
-        this.vanellope.x -= this.vanellope.blockSpeed; // 왼쪽으로 이동
-      } else if (this.moveState === 2) {
-        this.vanellope.y -= this.vanellope.blockSpeed; // 위로 이동
-      } else if (this.moveState === 3) {
-        this.vanellope.x -= this.vanellope.blockSpeed; // 다시 왼쪽으로 이동
-      }
-  
-      this.vanellope.currentDistance += this.vanellope.blockSpeed;
-  
-      if (this.vanellope.currentDistance >= this.maxDistance) {
-        this.moveState = (this.moveState + 1) % 4; // 이동 상태 변경
-        this.vanellope.currentDistance = 0; // 이동 거리 초기화
-      }
-  
-      this.vanellope.draw(this.context);
-  
-      // 빌런 블록들 이동 및 그리기
-      this.villains.forEach((villain) => {
-        if (this.moveState === 0) {
-          villain.y += villain.blockSpeed; // 아래로 이동
-        } else if (this.moveState === 1) {
-          villain.x -= villain.blockSpeed; // 왼쪽으로 이동
-        } else if (this.moveState === 2) {
-          villain.y -= villain.blockSpeed; // 위로 이동
-        } else if (this.moveState === 3) {
-          villain.x -= villain.blockSpeed; // 다시 왼쪽으로 이동
-        }
-  
-        villain.currentDistance += villain.blockSpeed;
-  
-        if (villain.currentDistance >= this.maxDistance) {
-          villain.currentDistance = 0; // 이동 거리 초기화
-        }
-        villain.draw(this.context);
-      });
-  
-      // 공과 vanellope 블록의 충돌 검사
-      this.balls.forEach((ball) => {
-        if (
-          this.vanellope.isHit(ball, this.items, this.increaseScore.bind(this))
-        ) {
-          this.vanellope.visible = true;
-          this.decreaseLife(); // 생명 하나 줄이기
-        }
-      });
-  
-      if (this.vanellope.x + this.vanellope.width < 0) {
-        alert('게임 승리');
-      }
-  
-      this.balls = this.balls.filter((ball) => {
-        ball.update(
-          this.canvas,
-          this.villains,
-          this.paddle,
-          this.items,
-          this.increaseScore.bind(this),
-          this.decreaseLife.bind(this)
-        );
-        return !ball.isRemoved;
-      });
-  
-      this.balls.forEach((ball) => {
-        ball.draw(this.context);
-        ball.update(
-          this.canvas,
-          this.villains,
-          this.paddle,
-          this.items,
-          this.increaseScore.bind(this),
-          this.decreaseLife.bind(this)
-        );
-      });
-  
-      this.paddle.draw(); // 막대기 그리기 추가
-  
-      this.lifes.forEach((life) => {
-        life.draw(); // 생명 그리기 추가
-      });
-  
-      this.items.forEach((item) => {
-        item.draw(this.context);
-        item.update(this.canvas);
-      });
-  
-      this.items.forEach((item) => {
-        this.balls.forEach((ball) => this.collectItem2(item, ball));
-      });
-  
-      // 바나나 그리기
-      this.bananas.forEach((banana) => {
-        banana.draw();
-        this.balls.forEach((ball) => {
-          if (banana.isColliding(ball)) {
-            console.log('충돌 발생'); // 충돌시 출력
-            ball.changeDirectionRandomly();
-          }
-        });
-      });
-  
-      this.drawScore(); // 점수 그리기 추가
-      requestAnimationFrame(update);
-    };
-    update();
-  }
-  
-  
-  
-
-  //곤용 -item2로 바꿨더니 작동함
-  collectItem2(item, ball) {
-    if (item.isPaddleGetItem(this.paddle)) {
-      console.log('아이템 수집: ' + item.type); // 디버그용 로그
-
-      if (item.type == 'increasevanellopespeed') {
-        this.increaseVanellopeSpeed();
-      } else if (item.type == 'decreasevanellopespeed') {
-        this.decreaseVanellopeSpeed();
-      } else if (item.type == 'increaseheart') {
-        this.increaseLife();
-      } else if (item.type == 'decreaseheart') {
-        this.decreaseLife();
-      } else if (item.type == 'increasemonsterspeed') {
-        this.increaseMonsterSpeed();
-      } else if (item.type == 'decreasemonsterspeed') {
-        this.decreaseMonsterSpeed();
-      }
-    }
   }
 
   // 바넬로피 속도 관련 함수
@@ -480,5 +335,26 @@ class Canvas2 extends Canvas {
         console.log('복원된 속도:', villain.blockSpeed); // 복원된 속도 디버깅 메시지
       });
     }, 2000);
+  }
+
+  // 아이템 수집 함수 수정
+  collectItem2(item, ball) {
+    if (item.isPaddleGetItem(this.paddle)) {
+      console.log('아이템 수집: ' + item.type); // 디버그용 로그
+
+      if (item.type == 'increasevanellopespeed') {
+        this.increaseVanellopeSpeed();
+      } else if (item.type == 'decreasevanellopespeed') {
+        this.decreaseVanellopeSpeed();
+      } else if (item.type == 'increaseheart') {
+        this.increaseLife();
+      } else if (item.type == 'decreaseheart') {
+        this.decreaseLife();
+      } else if (item.type == 'increasemonsterspeed') {
+        this.increaseMonsterSpeed();
+      } else if (item.type == 'decreasemonsterspeed') {
+        this.decreaseMonsterSpeed();
+      }
+    }
   }
 }
