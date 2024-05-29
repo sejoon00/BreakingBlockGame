d43df027087db091ba3c24dc5705aaba9b3c2388
김민석 <skyfox001015@naver.com>
2024-05-30 01:05:58 +0900

    feat : 게임2 빌런들 시작 위치 조정

--- game2/Canvas2.js
+++ game2/Canvas2.js
@@ -1,7 +1,7 @@
 class Canvas2 extends Canvas {
   constructor(backgroundimageUrl) {
     super(backgroundimageUrl);
-    this.canvas.id = "game2_canvas";
+    this.canvas.id = 'game2_canvas';
     this.vanellope;
     this.villains = []; // 빌런 리스트 초기화
     this.blockSpeed = 0.5; // 블록의 이동 속도
@@ -18,12 +18,12 @@ class Canvas2 extends Canvas {
       moveState: 0,
       currentDistance: 0,
     }; // 바넬로피의 상태 저장
-    window.addEventListener("keydown", this.handleKeyPress.bind(this));
+    window.addEventListener('keydown', this.handleKeyPress.bind(this));
   }
 
   initGameElements() {
     super.initGameElements(); // 부모 클래스의 initGameElements 호출
-    let startY = 116;
+    let startY = 80;
     let startX = 900; // 블록의 시작 x 위치
     this.vanellope = new Block(
       startX + 40,
@@ -31,7 +31,7 @@ class Canvas2 extends Canvas {
       100,
       80,
       this.increaseBrokenBlocks.bind(this),
-      "../game2/vanellope.png"
+      '../game2/vanellope.png'
     );
     this.vanellope.isVanellope = true;
     this.vanellope.blockSpeed = this.blockSpeed;
@@ -44,7 +44,7 @@ class Canvas2 extends Canvas {
         80,
         60,
         this.increaseBrokenBlocks.bind(this),
-        "../game2/villain" + i + ".png",
+        '../game2/villain' + i + '.png',
         false // 일반 블록임을 나타내는 플래그
       );
       villainBlock.blockSpeed = this.blockSpeed;
@@ -56,7 +56,7 @@ class Canvas2 extends Canvas {
       });
     }
 
-    const bananaImageSrc = "../source/banana.png";
+    const bananaImageSrc = '../source/banana.png';
     const bananaSize = 50;
     const yellowAreaTop = 100;
     const yellowAreaBottom = this.canvas.height - 150;
@@ -80,7 +80,7 @@ class Canvas2 extends Canvas {
           150,
           150,
           this.increaseBrokenBlocks.bind(this),
-          "../game2/boss.png"
+          '../game2/boss.png'
         );
         this.boss.blockSpeed = this.blockSpeed;
         this.boss.originalBlockSpeed = this.blockSpeed;
@@ -162,41 +162,41 @@ class Canvas2 extends Canvas {
   }
 
   handleKeyPress(event) {
-    if (event.key === "p" || event.key === "P") {
+    if (event.key === 'p' || event.key === 'P') {
       this.clearGame(); // 'P' 키를 누르면 게임 클리어
     }
   }
 
   clearGame() {
-    gameMode = "GameClear";
+    gameMode = 'GameClear';
 
     this.destroy();
     isGame2Cleared = true;
     toggleOverPage();
-    game3Img.src = "../stagePage/HeroDuty2.png";
+    game3Img.src = '../stagePage/HeroDuty2.png';
   }
 
   // 바넬로피가 먼저 도착
   endGame() {
-    let game3Img = document.querySelector("#game3Img");
+    let game3Img = document.querySelector('#game3Img');
     if (this.vanellope.x + this.vanellope.width < 0) {
-      console.log("GameClear");
-      gameMode = "GameClear";
+      console.log('GameClear');
+      gameMode = 'GameClear';
       this.destroy();
       isGame2Cleared = true;
       toggleOverPage();
-      game3Img.src = "../stagePage/HeroDuty2.png";
+      game3Img.src = '../stagePage/HeroDuty2.png';
     } else if (
       this.villains.some((villain) => villain.x + villain.width < 0) ||
       (this.boss && this.boss.x + this.boss.width < 0) ||
       this.balls.length === 0 ||
       this.lifes.length === 0
     ) {
-      console.log("GameOver");
-      gameMode = "GameOver";
+      console.log('GameOver');
+      gameMode = 'GameOver';
       this.destroy();
       toggleOverPage();
-      game3Img.src = "../stagePage/HeroDuty.png";
+      game3Img.src = '../stagePage/HeroDuty.png';
     }
   }
 
@@ -210,14 +210,14 @@ class Canvas2 extends Canvas {
         // 하얀색 반투명 배경으로 깜빡이기
         this.context.save();
         this.context.globalAlpha = 0.1;
-        this.context.fillStyle = "red";
+        this.context.fillStyle = 'red';
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.context.restore();
-        this.context.font = "48px Arial";
-        this.context.fillStyle = "red";
-        this.context.textAlign = "center";
+        this.context.font = '48px Arial';
+        this.context.fillStyle = 'red';
+        this.context.textAlign = 'center';
         this.context.fillText(
-          "Warning! 보스보다 먼저 도착하세요!",
+          'Warning! 보스보다 먼저 도착하세요!',
           this.canvas.width / 2,
           this.canvas.height / 2
         );
@@ -355,19 +355,19 @@ class Canvas2 extends Canvas {
 
   collectItem2(item, ball) {
     if (item.isPaddleGetItem(this.paddle)) {
-      console.log("아이템 수집: " + item.type); // 디버그용 로그
+      console.log('아이템 수집: ' + item.type); // 디버그용 로그
 
-      if (item.type === "increasevanellopespeed") {
+      if (item.type === 'increasevanellopespeed') {
         this.increaseVanellopeSpeed();
-      } else if (item.type === "decreasevanellopespeed") {
+      } else if (item.type === 'decreasevanellopespeed') {
         this.decreaseVanellopeSpeed();
-      } else if (item.type === "increaseheart") {
+      } else if (item.type === 'increaseheart') {
         this.increaseLife();
-      } else if (item.type === "decreaseheart") {
+      } else if (item.type === 'decreaseheart') {
         this.decreaseLife();
-      } else if (item.type === "increasemonsterspeed") {
+      } else if (item.type === 'increasemonsterspeed') {
         this.increaseMonsterSpeed();
-      } else if (item.type === "decreasemonsterspeed") {
+      } else if (item.type === 'decreasemonsterspeed') {
         this.decreaseMonsterSpeed();
       }
     }
