69ef45486c2a824b0ba6bb8fa378e3428742fff4
김민석 <skyfox001015@naver.com>
2024-05-29 21:12:21 +0900

    feat : game1일때만 변경

--- overPage/overPage.js
+++ overPage/overPage.js
@@ -2,8 +2,8 @@
 /* 해당 페이지의 HTML 코드를 작성하고 삽압하는 구간입니다. 주석이 잘 안보이니 자세히 확인부탁드립니다.*/
 
 //은서
-document.querySelector("main").insertAdjacentHTML(
-  "afterbegin",
+document.querySelector('main').insertAdjacentHTML(
+  'afterbegin',
   `
       <div id="overPage" class="popup">
         <div id="result"></div>
@@ -27,32 +27,32 @@ let gameover = document.querySelector("#gameover");
 // ---------------------------------- javascript function ----------------------------------
 /* 해당 페이지의 javascript에서 사용하는 function을 정의하는 구간입니다.*/
 function toggleOverPage() {
-  let str = "당신의 score는 " + canvas.score + "점입니다.";
-  if (gameMode.startsWith("Game")) {
-    if (document.querySelector("#result").innerHTML != "") {
-      document.querySelector("#result").innerHTML = "";
+  let str = '당신의 score는 ' + canvas.score + '점입니다.';
+  if (gameMode.startsWith('Game')) {
+    if (document.querySelector('#result').innerHTML != '') {
+      document.querySelector('#result').innerHTML = '';
     }
-    if ((gameclear.style.display = "block")) {
-      gameclear.style.display = "none";
+    if ((gameclear.style.display = 'block')) {
+      gameclear.style.display = 'none';
     }
-    if ((gameover.style.display = "block")) {
-      gameover.style.display = "none";
+    if ((gameover.style.display = 'block')) {
+      gameover.style.display = 'none';
     }
 
     change_position(overPage);
-    document.querySelector("#score").innerHTML = str;
-    overPage.style.display = "block";
+    document.querySelector('#score').innerHTML = str;
+    overPage.style.display = 'block';
 
     if (gameMode === "GameClear") {
       gameclear.style.display = "block";
       document.querySelector("#result").innerHTML = "GAME CLEAR";
 
-      if (gameState === "Gaming1" && isGame1Cleared == true) {
+      if (gameState === 'Gaming1') {
         setTimeout(() => {
           gotoLevelUpForGame1();
           isGameChanging = true;
         }, 3000);
-      } else if (gameState === "Gaming2" && isGame1Cleared === true) {
+      } else if (gameState === 'Gaming2') {
         setTimeout(() => {
           gotoLevelUpForGame2();
           isGameChanging = true;
@@ -76,19 +76,19 @@ function toggleOverPage() {
 function change_position() {
   var l = (window.innerWidth - 500) / 2;
   var t = (window.innerHeight - 400) / 2;
-  overPage.style.top = t + "px"; // CSS 속성 직접 설정
-  overPage.style.left = l + "px"; // CSS 속성 직접 설정
+  overPage.style.top = t + 'px'; // CSS 속성 직접 설정
+  overPage.style.left = l + 'px'; // CSS 속성 직접 설정
 }
 
 function gotoLevelUpForGame1() {
-  overPage.style.display = "none"; // 팝업창 종료
+  overPage.style.display = 'none'; // 팝업창 종료
   endGame1(); // 게임 종료
   Game1Audio.pause(); // 게임 bgm 종료
   document.querySelector("#levelUp1").style.display = "block"; // levelup1 페이지로 이동
   moveRalph(); // 애니메이션 함수 실행
 }
 function gotoLevelUpForGame2() {
-  overPage.style.display = "none"; // 팝업창 종료
+  overPage.style.display = 'none'; // 팝업창 종료
   endGame2(); // 게임 종료
   Game2Audio.pause(); // 게임 bgm 종료
   document.querySelector("#levelUp2").style.display = "block"; // levelup1 페이지로 이동
@@ -96,7 +96,7 @@ function gotoLevelUpForGame2() {
   moveKingCandy();
 }
 function gotoLevelUpForGame3() {
-  overPage.style.display = "none"; // 팝업창 종료
+  overPage.style.display = 'none'; // 팝업창 종료
   endGame3(); // 게임 종료
   Game3Audio.pause(); // 게임 bgm 종료
 
@@ -106,7 +106,7 @@ function gotoLevelUpForGame3() {
 function gameOverPlayAgain() {
   overPage.style.display = "none";
   // 게임 다시 시작
-  if (gameState == "Gaming1") {
+  if (gameState == 'Gaming1') {
     endGame1();
     setGame1();
   } else if (gameState == "Gaming2") {
